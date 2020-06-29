30ab7daa408c25b34d165a05ee9c03751803f343
AlexMielke1988 <52572112+AlexMielke1988@users.noreply.github.com>
2020-06-29 16:56:57 +0100

    submission

---
+++ Analyses Rhesus Interventions.R
@@ -0,0 +1,165 @@
+
+## load necessary packages
+library(brms)
+library(bayesplot)
+library(rstanarm)
+library(rstan)
+library(tidyverse)
+
+# load datasets
+load('data.RData')
+
+##### Model 1: Which grooming bouts face intervention?
+
+# Set priors
+m1priors <- c(
+  prior(student_t(3, 0, 2.5), class = "Intercept"),
+  prior(student_t(3, 0, 2.5), class = "b")
+)
+
+# Bayesian analysis using brms
+model1 = brm(
+  data = test.data.model1,
+  total.interventions ~ 1 +
+    z.rank.higher +
+    z.rank.lower +
+    z.dsi.groomer1.groomer2 +
+    kin.groomers +
+    z.bystanders +
+    (1 + z.rank.lower + z.dsi.groomer1.groomer2 + kin.groomers||higher) +
+    (1 + z.rank.higher + z.dsi.groomer1.groomer2 + kin.groomers||lower) +
+    offset(log(duration)),
+  prior = m1priors,
+  warmup = 500,
+  iter = 3000,
+  cores = 10,
+  chains = 2,
+  family = bernoulli(link = 'logit'),
+  seed = 123
+)
+
+summary(model1)
+r2.intervention = bayes_R2(model1)
+odds.model1 = exp(fixef(model1))
+
+###### Model 2: Who intervened?
+
+# conditional logistic regression using stan_clogit
+
+model2 = stan_clogit(
+  intervention ~ 1 +
+    z.rank.pot.intervener +
+    z.rank.diff.higher +
+    z.rank.diff.lower +
+    z.dsi.higher.intervener +
+    z.dsi.lower.intervener +
+    kin.higher.intervener +
+    kin.lower.intervener +
+    (1|pot.intervener),
+  strata = gr.bout.id,
+  data = test.data.model2,
+  prior = student_t(),
+  QR = TRUE,
+  algorithm = 'sampling',
+  chains = 2,
+  warmup = 500,
+  iter = 3000,
+  cores = 10,
+  sparse = FALSE
+)
+
+
+summary(model2,
+        probs = c(0.025, 0.5, 0.975),
+        digits = 2)
+
+r2.intervener = mean(bayes_R2(model2))
+odds.model2 = exp(fixef(model2))
+
+
+###### Model 3: which of the two groomers was chosen as target?
+
+# conditional logistic regression using stan_clogit
+model3 = stan_clogit(
+  choice ~
+    z.rank.target +
+    z.dsi.target.intervener +
+    kin.target.intervener +
+    (1 + z.dsi.target.intervener + kin.target.intervener || target),
+  strata = bout,
+  data = test.data.model3,
+  prior = student_t(),
+  QR = T,
+  algorithm = 'sampling',
+  chains = 2,
+  iter = 3000,
+  cores = 10,
+  sparse = F
+)
+
+summary(model3,
+        probs = c(0.025, 0.5, 0.975),
+        digits = 2)
+
+r2.target = mean(bayes_R2(model3))
+odds.model3 = exp(fixef(model3))
+
+
+########################### Model 4.1: Which groomers stayed, which ones left
+
+# set priors
+m1priors <- c(
+  prior(student_t(3, 0, 2.5), class = "Intercept"),
+  prior(student_t(3, 0, 2.5), class = "b")
+)
+
+# Bayesian analysis using brms
+model41 = brm(
+  data = test.data.model41,
+  stay ~ 1 +
+    z.rank.target * z.rank.intervener +
+    kin.target.intervener +
+    z.dsi.target.intervener +
+    (1 + z.rank.intervener + z.dsi.target.intervener|| target) +
+    (1 + z.rank.target + z.dsi.target.intervener|| intervener),
+  prior = m1priors,
+  warmup = 500,
+  iter = 3000,
+  cores = 10,
+  chains = 2,
+  family = bernoulli(link = 'logit'),
+  seed = 123
+)
+
+summary(model41)
+r2.outcome41 = bayes_R2(model41)
+odds.model41 = exp(fixef(model41))
+
+########################### Model 4.2: Did the intervener gain access?
+
+# set priors
+m1priors <- c(
+  prior(student_t(3, 0, 2.5), class = "Intercept"),
+  prior(student_t(3, 0, 2.5), class = "b")
+)
+
+# Bayesian analysis using brms
+model42 = brm(
+  data = test.data.model42,
+  outcome ~
+    kin.either +
+    z.rank.intervener +
+    z.max.dsi +
+    (1|intervener) +
+    (1|target) +
+    (1|competitor),
+  warmup = 500,
+  iter = 5000,
+  prior = m1priors,
+  cores = 10,
+  chains = 2,
+  family = categorical(refcat = 'ignored'),
+  seed = 123
+)
+summary(model42)
+odds.model42 = exp(fixef(model42))
